fcf37f6f200721e128c555c5419442a1653bc0e3
Christopher Denny <chris.denny@utexas.edu>
2020-04-12 21:57:12 -0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "start"
+echo "Update package cache"
+sudo apt update
+
+echo "Downloading programs"
+sudo apt install vim tmux htop
+
+echo "Pulling dotfiles"
+wget https://raw.githubusercontent.com/christodenny/dotfiles/master/.vimrc -O ~/.vimrc
+wget https://raw.githubusercontent.com/christodenny/dotfiles/master/.tmux.conf -O ~/.tmux.conf
